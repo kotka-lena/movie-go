8e9a589896c6ad1ea5045677cbff737aa1bfb9eb
Elena Vasiljeva <e.a.vasiljeva@gmail.com>
2020-04-21 16:21:05 +0700

    Optimize subscriptions to "Favorites movies" list

--- src/app/shared/components/favorites-button/favorites-button.component.ts
+++ src/app/shared/components/favorites-button/favorites-button.component.ts
@@ -1,50 +1,34 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
-import {Subscription} from 'rxjs';
+import {Component, Input, OnChanges, OnDestroy, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
 import {FavouritesService} from '../../favourites.service';
-import {MoviesService} from '../../movies.service';
 
 @Component({
   selector: 'app-favorites-button',
   templateUrl: './favorites-button.component.html',
   styleUrls: ['./favorites-button.component.scss']
 })
-export class FavoritesButtonComponent implements OnInit, OnDestroy {
+export class FavoritesButtonComponent implements OnChanges, OnInit, OnDestroy {
 
   @Input() movieId: number;
   inFavourites: boolean;
-  fSub: Subscription;
-  mSub: Subscription;
+  changes$: Subject<boolean>;
 
   constructor(
-    public favService: FavouritesService,
-    private moviesService: MoviesService
+    public favService: FavouritesService
   ) { }
 
-  ngOnInit(): void {
+  ngOnChanges() {
     this.inFavourites = this.favService.inFavourites(this.movieId);
-    this.fSub = this.favService.onChange$.subscribe((movieId: number) => {
-      if (movieId === this.movieId) {
-        this.inFavourites = this.favService.inFavourites(this.movieId);
-      }
-    });
-    window.addEventListener('storage', (event) => {
-      if (event.key === 'fav') {
-        this.inFavourites = this.favService.inFavourites(this.movieId);
-      }
-    });
-    this.mSub = this.moviesService.movieActivated$.subscribe((movieId: number) => {
-      if (movieId === this.movieId) {
-        this.inFavourites = this.favService.inFavourites(movieId);
-      }
+    this.changes$ = this.favService.subscribeToChanges(this.movieId);
+    this.changes$.subscribe(() => {
+      this.inFavourites = this.favService.inFavourites(this.movieId);
     });
   }
 
+  ngOnInit(): void { }
+
   ngOnDestroy() {
-    if (this.fSub) {
-      this.fSub.unsubscribe();
-    }
-    if (this.mSub) {
-      this.mSub.unsubscribe();
-    }
+    this.favService.unsubscribeFromChanges(this.movieId, this.changes$);
+    this.changes$.unsubscribe();
   }
 }
--- src/app/shared/favourites.service.ts
+++ src/app/shared/favourites.service.ts
@@ -1,11 +1,18 @@
-import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs';
+import {Injectable, OnDestroy} from '@angular/core';
+import {Subject, Subscription} from 'rxjs';
+import {MoviesService} from './movies.service';
 
 @Injectable({providedIn: 'root'})
-export class FavouritesService {
+export class FavouritesService implements OnDestroy {
 
-  favoriteMovieIds: number[];
+  favouriteMovieIds: number[];
+  subscribersByMovieId: Map<number, Subject<boolean>[]> = new Map();
+  movieSub: Subscription;
   onChange$: Subject<number> = new Subject<number>();
+
+  constructor(private moviesService: MoviesService) {
+    this.subscribeToAllChanges();
+  }
 
   inFavourites(movieId: number) {
     for (const id of this.getAll()) {
@@ -16,30 +23,81 @@ export class FavouritesService {
     return false;
   }
 
+  subscribeToChanges(movieId: number): Subject<boolean> {
+    const subject$: Subject<boolean> = new Subject<boolean>();
+    if (this.subscribersByMovieId.has(movieId)) {
+      this.subscribersByMovieId.get(movieId).push(subject$);
+    }
+    else {
+      this.subscribersByMovieId.set(movieId, [subject$]);
+    }
+    return subject$;
+  }
+
+  unsubscribeFromChanges(movieId: number, subjectForMovieId$: Subject<boolean>) {
+    const index = this.subscribersByMovieId.get(movieId).indexOf(subjectForMovieId$);
+    if (index !== -1) {
+      this.subscribersByMovieId.get(movieId).splice(index, 1);
+    }
+  }
+
   add(movieId: number) {
-    this.getAll().push(movieId);
+    this.getAll().unshift(movieId);
     this.set();
     this.onChange$.next(movieId);
+    this.nextSubjectForMovieId(movieId);
   }
 
   remove(movieId: number) {
     const index = this.getAll().indexOf(movieId);
     if (index !== -1) {
-      this.favoriteMovieIds.splice(index, 1);
+      this.favouriteMovieIds.splice(index, 1);
     }
     this.set();
     this.onChange$.next(movieId);
+    this.nextSubjectForMovieId(movieId);
   }
 
   getAll(): number[] {
     const maybeIdsString = localStorage.getItem('fav');
-    this.favoriteMovieIds = (maybeIdsString !== null) ? JSON.parse(maybeIdsString) : [];
-    return this.favoriteMovieIds;
+    this.favouriteMovieIds = (maybeIdsString !== null) ? JSON.parse(maybeIdsString) : [];
+    return this.favouriteMovieIds;
+  }
+
+  changedMovieIds(): number[] {
+    const maybeNewIdsString = localStorage.getItem('fav');
+    const newFavoriteMovieIds = (maybeNewIdsString !== null) ? JSON.parse(maybeNewIdsString) : [];
+    return this.favouriteMovieIds.filter(id => !newFavoriteMovieIds.includes(id))
+      .concat(newFavoriteMovieIds.filter(id => !this.favouriteMovieIds.includes(id)));
+  }
+
+  subscribeToAllChanges() {
+    if (!this.movieSub) {
+      console.log('subscribeToAllChanges');
+      this.movieSub = this.moviesService.movieActivated$.subscribe((movieId: number) => {
+        this.nextSubjectForMovieId(movieId);
+      });
+    }
+    window.addEventListener('storage', (event) => {
+      if (event.key === 'fav') {
+        for (const movieId of this.changedMovieIds()) {
+          this.nextSubjectForMovieId(movieId);
+        }
+      }
+    });
+  }
+
+  nextSubjectForMovieId(movieId: number) {
+    if (this.subscribersByMovieId.get(movieId) !== undefined) {
+      for (const subject$ of this.subscribersByMovieId.get(movieId)) {
+        subject$.next(true);
+      }
+    }
   }
 
   set() {
     localStorage.removeItem('fav');
-    localStorage.setItem('fav', JSON.stringify(this.favoriteMovieIds));
+    localStorage.setItem('fav', JSON.stringify(this.favouriteMovieIds));
   }
 
   clear() {
@@ -47,6 +105,13 @@ export class FavouritesService {
     localStorage.removeItem('fav');
     for (const movieId of movieIds) {
       this.onChange$.next(movieId);
+      this.nextSubjectForMovieId(movieId);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.movieSub) {
+      this.movieSub.unsubscribe();
     }
   }
 }
